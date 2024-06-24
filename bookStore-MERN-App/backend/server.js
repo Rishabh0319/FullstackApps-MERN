42da5b6268f374068c00c6aefa213360ab13c4a9
Rishabh0319 <rishabhtripathi0319@gmail.com>
2024-06-24 10:33:16 +0530

    bookStore-MERN-App/  Basic Server setup and database connection is Done

---
+++ bookStore-MERN-App/backend/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import { PORT, mongoDBURL } from './config.js';
+import mongoose from "mongoose";
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.json({ msg: "Hello From Nodejs Server" })
+})
+
+mongoose.connect(mongoDBURL)
+    .then(() => {
+        console.log('Server is now connected to Database');
+        app.listen(PORT, () => {
+            console.log(`Server is Running on Port:${PORT}`);
+        });
+    })
+    .catch(() => {
+
+    });
