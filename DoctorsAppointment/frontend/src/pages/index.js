8d8f748d4b013249a2040c3841d8bcd571dc5819
Rishabh0319 <rishabhtripathi0319@gmail.com>
2024-09-11 22:45:11 +0530

    DoctorsAppointment -> basic file structure, pages components, routes setup

---
+++ DoctorsAppointment/frontend/src/pages/index.js
@@ -0,0 +1,17 @@
+import About from './About'
+import Contact from './Contact'
+import Doctors from './Doctors'
+import Home from './Home'
+import Login from './Login'
+import MyAppointments from './MyAppointments'
+import MyProfile from './MyProfile'
+
+export {
+   About,
+   Contact,
+   Doctors,
+   Home,
+   Login,
+   MyAppointments,
+   MyProfile
+}
